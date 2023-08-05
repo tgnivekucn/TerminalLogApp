d8b25169a9e24892aba0bea4b456ceb3d2596ea7
KuangYu <tgnivekucn@gmail.com>
2023-08-05 22:50:58 +0800

    Add test case1 & case 2 for testing read file with fixed content and the file be updated dynamically

--- TerminalLogApp/TerminalLogApp/ViewController.swift
+++ TerminalLogApp/TerminalLogApp/ViewController.swift
@@ -13,8 +13,63 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("test11 do action")
+        testCase1()
+        
     }
 
+    private func testCase1() {
+        var testFilePath2: URL? {
+            guard let filePath = Bundle.main.path(forResource: "log", ofType: "txt") else {
+                return nil
+            }
+            return URL(string: filePath)
+        }
+        if let filePath = testFilePath2 {
+            ReadFileManager.shared.asyncReadFile(atPath: filePath.path, readTextCallback: { text in
+//                print("test11 text: \(text)")
+            })
+        }
+    }
 
+    private func testCase2() {
+        var testDir: URL? {
+            if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                let subDirURL = dir.appendingPathComponent("TestDir")
+                if FileManager.default.fileExists(atPath: subDirURL.path) {
+                    return subDirURL
+                } else {
+                    do {
+                        try FileManager.default.createDirectory(atPath: subDirURL.path, withIntermediateDirectories: true, attributes: nil)
+                        return subDirURL
+                    } catch {
+                        return nil
+                    }
+                }
+            }
+            return nil
+        }
+
+        var testFilePath: URL? {
+            guard let testDir = testDir else {
+                return nil
+            }
+            let filePath = testDir.appendingPathComponent("test.txt")
+            return filePath
+        }
+        
+        if let filePath = testFilePath {
+            Utilities.deleteTestFile(testFilePath: filePath)
+            Utilities.startTest(testFilePath: filePath)
+            ReadFileManager.shared.asyncReadFile(atPath: filePath.path, readTextCallback: { text in
+//                print("test11 text: \(text)")
+            })
+            
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                WriteFileManager.shared.asyncWriteFile(atPath: filePath.path, withText: "", withLineBreak: true, withDateStringPrefix: true) { result in
+                    print("test11 write text success!!")
+                }
+            }
+        }
+    }
 }
 
